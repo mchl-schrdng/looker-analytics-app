b320fb72b5a7a22584ae3f78d0887ccd45830714
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-03-02 08:30:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,156 +1,136 @@
 import os
 import streamlit as st
-import pandas as pd
+import plotly.graph_objs as go
 import looker_sdk
-from looker_sdk import models40
+import json
+from PIL import Image
+from plotly.subplots import make_subplots
+from datetime import datetime
 
-# Set Looker SDK environment variables using GitHub secrets
-os.environ["LOOKERSDK_BASE_URL"] = "https://mylookerinstance.eu.looker.com/"
+# Load logo image
+logo = Image.open("logo.png").resize((100, 100))
+
+# Set page configuration with logo in top left corner
+st.set_page_config(page_title='My Little App', page_icon=logo, layout='wide')
+
+# Display logo in left sidebar
+st.sidebar.image(logo)
+
+# Set Looker SDK environment variables
+os.environ["LOOKERSDK_BASE_URL"] = "https://xxx.eu.looker.com/"
 os.environ["LOOKERSDK_API_VERSION"] = "4.0"
 os.environ["LOOKERSDK_VERIFY_SSL"] = "true"
 os.environ["LOOKERSDK_TIMEOUT"] = "120"
-os.environ["LOOKERSDK_CLIENT_ID"] = "client_id"
-os.environ["LOOKERSDK_CLIENT_SECRET"] = "client_secret"
+os.environ["LOOKERSDK_CLIENT_ID"] = "xxx"
+os.environ["LOOKERSDK_CLIENT_SECRET"] = "xxx"
 
-def main():
-    st.header("My Looker Micro Front")
-    st.markdown("---")
+# Authenticate with Looker API
+sdk = looker_sdk.init31()
 
-    st.title("User Management")
-
-    # Authenticate with Looker SDK
-    sdk = looker_sdk.init31()
-
-    # Define a collapsible section for adding users to groups
-    user_management_expander = st.expander("Add Users to Groups", expanded=False)
-    with user_management_expander:
-        # Call Looker SDK to get all users
-        all_users = sdk.all_users()
-        user_info = []
-        for user in all_users:
-            user_info.append({
-                "ID": user.id,
-                "Email": user.email,
-                "First Name": user.first_name,
-                "Last Name": user.last_name,
-                "Disabled": user.is_disabled
-            })
-
-        # Display all users in a table with checkboxes to select users
-        user_info_df = pd.DataFrame(user_info)
-        user_info_df['Option'] = user_info_df['ID'].astype(str) + ' - ' + user_info_df['First Name'] + ' ' + user_info_df['Last Name'] + ' (' + user_info_df['Email'] + ')'
-        selected_user_options = user_info_df.set_index('ID')['Option'].to_dict()
-        selected_user_ids = st.multiselect("Select users to add to groups", options=list(selected_user_options.keys()), format_func=lambda x: selected_user_options[x])
-        selected_users_df = user_info_df[user_info_df['ID'].isin(selected_user_ids)]
-        st.dataframe(selected_users_df)
-
-
-        # Call Looker SDK to get all groups
-        all_groups = sdk.all_groups()
-        group_info = []
-        for group in all_groups:
-            group_info.append({
-                "ID": group.id,
-                "Name": group.name,
-                "User Count": group.user_count
+# Define Looker query function
+@st.cache_data(ttl=3600) # cache data for 1 hour
+def get_look_data():
+    response = sdk.run_query(
+        query_id="6219",
+        result_format="json"
+    )
+    data = json.loads(response)
+    result = []
+    for row in data:
+        result.append({
+            'date': row['order_items.created_date'],
+            'count': row['order_items.count'],
+            'total_sale_price': row['order_items.total_sale_price'],
+            'avg_sale_price': row['order_items.avg_sale_price'],
         })
+    return result
 
-        # Display all groups in a table with checkboxes to select groups
-        group_info_df = pd.DataFrame(group_info)
-        group_info_df['Option'] = group_info_df['ID'].astype(str) + ' - ' + group_info_df['Name']
-        selected_group_options = group_info_df.set_index('ID')['Option'].to_dict()
-        selected_group_ids = st.multiselect("Select groups to add users to", options=list(selected_group_options.keys()), format_func=lambda x: selected_group_options[x])
-        selected_groups_df = group_info_df[group_info_df['ID'].isin(selected_group_ids)]
-        st.dataframe(selected_groups_df)
+# Get Looker data
+result = get_look_data()
 
-        # Add a button to add selected users to selected groups
-        if st.button("Add Users to Selected Groups"):
-            if len(selected_user_ids) == 0 or len(selected_group_ids) == 0:
-                st.warning("Please select at least one user and one group.")
-            else:
-                # Call Looker SDK to add users to groups
-                for group_id in selected_group_ids:
-                    for user_id in selected_user_ids:
-                        sdk.add_group_user(group_id=group_id, body=models40.GroupIdForGroupUserInclusion(user_id=user_id))
+# Get the minimum and maximum dates in the data
+min_date = datetime.strptime(result[0]['date'], '%Y-%m-%d')
+max_date = datetime.strptime(result[-1]['date'], '%Y-%m-%d')
 
-                st.success(f"Selected users ({len(selected_user_ids)}) added to selected groups ({len(selected_group_ids)}).")
+# Set default value for slider to the first and last dates in the result
+start_time = st.sidebar.slider(
+    "Date picker",
+    min_value=min_date,
+    max_value=max_date,
+    value=[min_date, max_date],
+    format="MM/DD/YY"
+)
 
-    # Define a collapsible section for removing users from groups
-    user_removal_expander = st.expander("Remove Users from Group", expanded=False)
-    with user_removal_expander:
-        # Call Looker SDK to get all groups
-        all_groups = sdk.all_groups()
-        group_info = []
-        for group in all_groups:
-            group_info.append({
-                "ID": group.id,
-                "Name": group.name,
-                "User Count": group.user_count
-         })
+# Filter data based on slider value
+filtered_data = [row for row in result if datetime.strptime(row['date'], '%Y-%m-%d') >= start_time[0] and datetime.strptime(row['date'], '%Y-%m-%d') <= start_time[1]]
 
-        # Display all groups in a table with checkboxes to select a group to remove users from
-        group_info_df = pd.DataFrame(group_info)
-        group_info_df['Option'] = group_info_df['ID'].astype(str) + ' - ' + group_info_df['Name']
-        selected_group_options = group_info_df.set_index('ID')['Option'].to_dict()
-        selected_group_id = st.selectbox("Select a group to remove users from", options=selected_group_options, format_func=lambda x: selected_group_options[x])
-        selected_group_df = group_info_df[group_info_df['ID'] == selected_group_id]
-        st.dataframe(selected_group_df)
+# Create plotly figure for total sale price and count of order items
+fig1 = go.Figure()
+fig1.update_layout(height=300, width=400)
+fig1.add_trace(go.Scatter(x=[row['date'] for row in filtered_data], y=[row['total_sale_price'] for row in filtered_data], fill='tozeroy', fillcolor='rgba(255, 0, 0, 0.5)', name='Total Sale Price'))
+fig1.update_layout(title='Total Sale Price', xaxis_title='Date', yaxis_title="Total Sale Price")
 
-        # Call Looker SDK to get all users in the selected group
-        response = sdk.all_group_users(group_id=selected_group_id)
-        user_info = []
-        for user in response:
-            user_info.append({
-                "ID": user.id,
-                "Email": user.email,
-                "First Name": user.first_name,
-                "Last Name": user.last_name,
-                "Disabled": user.is_disabled
-            })
+fig2 = go.Figure()
+fig2.update_layout(height=300, width=400)
+fig2.add_trace(go.Scatter(x=[row['date'] for row in filtered_data], y=[row['count'] for row in filtered_data], fill='tozeroy', fillcolor='rgba(255, 0, 0, 0.5)', name='Count of Order Items'))
+fig2.update_layout(title='Count of Order Items', xaxis_title='Date', yaxis_title="Count of Order Items")
 
-        # Display all users in the selected group in a table with checkboxes to select users to remove
-        user_info_df = pd.DataFrame(user_info)
-        user_info_df['Option'] = user_info_df['ID'].astype(str) + ' - ' + user_info_df['First Name'] + ' ' + user_info_df['Last Name'] + ' (' + user_info_df['Email'] + ')'
-        selected_user_options = user_info_df.set_index('ID')['Option'].to_dict()
-        selected_user_ids = st.multiselect("Select users to remove from the group", options=list(selected_user_options.keys()), format_func=lambda x: selected_user_options[x])
-        selected_users_df = user_info_df[user_info_df['ID'].isin(selected_user_ids)]
-        st.dataframe(selected_users_df)
+# Create plotly figure for sum of sales
+sales_sum = sum([row['total_sale_price'] for row in filtered_data])
+fig3 = go.Figure()
+fig3.update_layout(height=200, width=400)
+fig3.add_trace(go.Indicator(
+    mode = "number",
+    value = sales_sum,
+    number={'prefix': "$",},
+    title = {"text": "Total Sales"}
+))
+fig3.update_layout(title='Sales Metrics')
 
-        # Add a button to remove selected users from the selected group
-        if st.button("Remove Users from Selected Group"):
-            if len(selected_user_ids) == 0:
-                st.warning("Please select at least one user to remove.")
-            else:
-                # Call Looker SDK to remove users from the selected group
-                for user_id in selected_user_ids:
-                    sdk.delete_group_user(
-                        group_id=selected_group_id,
-                        user_id=user_id
-                    )
+# Create plotly figure for sum of count of orders
+orders_sum = sum([row['count'] for row in filtered_data])
+fig4 = go.Figure()
+fig4.update_layout(height=200, width=400)
+fig4.add_trace(go.Indicator(
+    mode = "number",
+    value = orders_sum,
+    title = {"text": "Total Orders"}
+))
+fig4.update_layout(title='Orders Metrics')
 
-                st.success(f"Selected users ({len(selected_user_ids)}) removed from group ({selected_group_id}).")
- 
-    st.title("Datagroups")
+# Define Looker query function for order items by status
+def get_order_status_data():
+    response = sdk.run_query(
+        query_id="6253",
+        result_format="json"
+    )
+    data = json.loads(response)
+    result = {}
+    for row in data:
+        status = row['order_items.status']
+        date = datetime.strptime(row['order_items.created_date'], '%Y-%m-%d')
+        count = row['order_items.count']
+        if date >= start_time[0] and date <= start_time[1]:
+            result[status] = result.get(status, 0) + count
+    return result
 
-    # Define a collapsible section for datagroups
-    datagroups_expander = st.expander("Datagroups status", expanded=False)
-    with datagroups_expander:
-        # Call Looker SDK to get all datagroups
-        all_datagroups = sdk.all_datagroups()
-        datagroup_info = []
-        for datagroup in all_datagroups:
-            datagroup_info.append({
-                "ID": datagroup.id,
-                "Model Name": datagroup.model_name,
-                "Name": datagroup.name,
-                "Trigger Check At": datagroup.trigger_check_at,
-                "Trigger Error": datagroup.trigger_error,
-                "Trigger Value": datagroup.trigger_value
-            })
+# Get Looker data
+order_status_data = get_order_status_data()
 
-        # Display all datagroups in a table
-        datagroup_info_df = pd.DataFrame(datagroup_info, columns=["ID", "Model Name", "Name", "Trigger Check At", "Trigger Error", "Trigger Value"])
-        st.dataframe(datagroup_info_df)
+# Create plotly figure for order items by status
+labels = list(order_status_data.keys())
+values = list(order_status_data.values())
+fig5 = go.Figure(data=[go.Pie(labels=labels, values=values)])
+fig5.update_traces(hole=.4, hoverinfo="label+percent+name")
+fig5.update_layout(title='Order Items by Status')
 
-if __name__ == "__main__":
-    main()
+# Display plotly figures side by side in Streamlit app
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.plotly_chart(fig3, use_container_width=True)
+    st.plotly_chart(fig1, use_container_width=True)
+with col2:
+    st.plotly_chart(fig4, use_container_width=True)
+    st.plotly_chart(fig2, use_container_width=True)
+with col3:
+    st.plotly_chart(fig5, use_container_width=True)
